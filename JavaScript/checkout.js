275e054891f2bcafbdd919e3ae1f4eb4a00c02e3
666RED <hongchensee8@gmail.com>
2022-12-16 21:57:24 +0800

    added remove button

--- JavaScript/checkout.js
+++ JavaScript/checkout.js
@@ -2,6 +2,7 @@ const decreaseButtons = document.querySelectorAll('.decrease-icon');
 const increaseButtons = document.querySelectorAll('.increase-icon');
 const removeYesBtn = document.querySelector('.remove-product-yes-btn');
 const removeNoBtn = document.querySelector('.remove-product-no-btn');
+const removeButtons = document.querySelectorAll('.remove-icon');  
 const removePopupMenu = document.querySelector('.remove-product-pop-up');
 const removeCancel = document.querySelector('.remove-product-cancel-img');
 
@@ -40,5 +41,19 @@ increaseButtons.forEach(button => {
 
     const productQuantity = adjustQuantityRow.querySelector('.quantity');
     productQuantity.innerText++;
+  });
+});
+
+removeButtons.forEach(button => {
+  button.addEventListener('click', (e) => {
+    const buttonClicked = e.target;
+    const adjustQuantityRow = buttonClicked.parentElement;
+
+    removePopupMenu.classList.add('open-popup');
+    removeYesBtn.addEventListener('click', (e) => {
+      removePopupMenu.classList.remove('open-popup');
+      const productRow = adjustQuantityRow.parentElement;
+      productRow.remove();
+    });
   });
 });
