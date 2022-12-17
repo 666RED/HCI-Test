d22d42ba3dda1fa260505c1cd4fb6e39b78af1d1
666RED <hongchensee8@gmail.com>
2022-12-17 23:06:50 +0800

    update checkout

--- JavaScript/inventory.js
+++ JavaScript/inventory.js
@@ -1,12 +1,19 @@
 const editButtons = document.querySelectorAll('.edit-btn');
+const productNames = document.querySelectorAll('.product-name');
 
 const itemArr = [];
 
 editButtons.forEach(button => {
   button.addEventListener('click', (e) => {
-    const buttonClicked = e.target;
-    const itemContainer = buttonClicked.parentElement.parentElement;
-    
+    // const buttonClicked = e.target;
+    // const itemContainer = buttonClicked.parentElement.parentElement;
+    window.location.href = 'edit-item.html';
     // const 
+  })
+});
+
+productNames.forEach(name => {
+  name.addEventListener('click', () => {
+    window.location.href = 'edit-item.html';
   })
 });
