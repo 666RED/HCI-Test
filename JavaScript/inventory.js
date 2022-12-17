4f90f8733895bc0b06c5b3530823762c717280de
666RED <hongchensee8@gmail.com>
2022-12-17 22:52:50 +0800

    update inventory

---
+++ JavaScript/inventory.js
@@ -0,0 +1,12 @@
+const editButtons = document.querySelectorAll('.edit-btn');
+
+const itemArr = [];
+
+editButtons.forEach(button => {
+  button.addEventListener('click', (e) => {
+    const buttonClicked = e.target;
+    const itemContainer = buttonClicked.parentElement.parentElement;
+    
+    // const 
+  })
+});
