37f88ef25a0e533cd175d58b23b9cb3e13567315
666RED <hongchensee8@gmail.com>
2022-12-26 22:55:48 +0800

    update restock interface

--- JavaScript/restock.js
+++ JavaScript/restock.js
@@ -1,5 +1,14 @@
 const returnIcon = document.querySelector('.return-icon');
+const contentContainer = document.querySelector('.content-container');
+const productPrices = document.querySelectorAll('.product-price');
 
 returnIcon.addEventListener('click', () => {
   window.location.href = 'inventory.html';
 });
+
+if(contentContainer.offsetHeight >= 256){
+  productPrices.forEach(price => {
+    price.style.width = '110px';
+    price.style.paddingLeft = '18px';
+  });
+}
